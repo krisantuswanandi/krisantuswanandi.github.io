7e0a9d02e3cd6c70bd2702a989ea26cf344547fa
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-02-07 23:55:49 +0700

    Add mobile view

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -30,11 +30,10 @@ export const Homepage = component$(() => {
   ]
   
   return (
-    <div class="h-full flex items-center p-24 bg-neutral-800 text-5xl">
-      <div>
-        <div class="mb-6">
-          <span>krisantus wanandi</span>
-          <span class="inline-block ml-2 text-7xl text-rose-500">.</span>
+    <div class="h-full flex items-center justify-center sm:justify-start p-4 sm:p-24 bg-neutral-800">
+      <div class="flex flex-col items-center sm:items-start">
+        <div class="mb-6 -mr-3 text-2xl sm:text-5xl after:content-['.'] after:ml-1 after:sm:ml-2 after:text-4xl after:sm:text-7xl after:text-rose-500">
+          krisantus wanandi
         </div>
         <div class="flex items-center gap-6">
           {socials.map(social => (
--- src/components/homepage/social.tsx
+++ src/components/homepage/social.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik'
 export default component$(({ img, url }: { img: string; url: string }) => {
   return (
     <a href={url} target="_blank" class="opacity-25 hover:opacity-70">
-      <img src={img} alt="social icon" class="w-6" />
+      <img src={img} alt="social icon" class="w-4 sm:w-6" />
     </a>
   )
 })
