0205cd058ea90d1bc4b07feed7ac1719709e568c
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-02-12 20:29:18 +0700

    add light mode

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -10,33 +10,33 @@ export const Homepage = component$(() => {
     {
       id: 'github',
       icon: GithubIcon,
-      url: 'https://github.com/krisantuswanandi'
+      url: 'https://github.com/krisantuswanandi',
     },
     {
       id: 'twitter',
       icon: TwitterIcon,
-      url: 'https://twitter.com/kris_antus'
+      url: 'https://twitter.com/kris_antus',
     },
     {
       id: 'codepen',
       icon: CodepenIcon,
-      url: 'https://codepen.io/krisantuswanandi'
+      url: 'https://codepen.io/krisantuswanandi',
     },
     {
       id: 'instagram',
       icon: InstagramIcon,
-      url: 'https://instagram.com/krisantus.wanandi'
+      url: 'https://instagram.com/krisantus.wanandi',
     },
   ]
-  
+
   return (
-    <div class="h-full flex items-center justify-center sm:justify-start p-4 sm:p-24 bg-neutral-800">
+    <div class="flex h-full items-center justify-center bg-neutral-100 p-4 dark:bg-neutral-800 sm:justify-start sm:p-24">
       <div class="flex flex-col items-center sm:items-start">
-        <div class="mb-6 -mr-3 text-2xl sm:text-5xl after:content-['.'] after:ml-1 after:sm:ml-2 after:text-4xl after:sm:text-7xl after:text-rose-500">
+        <div class="mb-6 -mr-3 text-2xl text-neutral-800 after:ml-1 after:text-4xl after:text-rose-500 after:content-['.'] dark:text-neutral-100 sm:text-5xl sm:after:ml-2 sm:after:text-7xl">
           krisantus wanandi
         </div>
         <div class="flex items-center gap-6">
-          {socials.map(social => (
+          {socials.map((social) => (
             <Social key={social.id} img={social.icon} url={social.url} />
           ))}
         </div>
